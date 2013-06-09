1a1fc411971e3eef29db6d4dc074c8183f9ab04d
Indrajith K L <mac91112@gmail.com>
2013-06-09 22:04:00 +0530

    redraw region solved
    Hurray!

--- 07-06-2013/Banner.java
+++ 07-06-2013/Banner.java
@@ -1,6 +1,9 @@
 import java.applet.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 /*
 <applet code="Banner" width=500 height=500>
 <param name="name" value="Vivek Vijayan">
@@ -9,7 +12,7 @@ import java.awt.event.*;
 <param name="phone" value="+919446067530">
 </applet>
 */
-public class Banner extends Applet implements Runnable
+public class Banner extends Applet implements Runnable//,MouseListener,MouseMotionListener
 {
 	String msg=" Software Trainning and Development Center  ";
 	Thread t=null;
@@ -20,6 +23,8 @@ public class Banner extends Applet implements Runnable
 	boolean stopFlag;
 	public void init()
 	{
+		/*addMouseListener(this);
+        addMouseMotionListener(this);*/
 		setBackground(cob);
 		setForeground(Color.white);
 		name = getParameter("name");
@@ -42,8 +47,8 @@ public class Banner extends Applet implements Runnable
 		{
 			try
 			{
-				//repaint(0,310,100,100);
-				repaint();
+				repaint(304,103,664,47);
+				//repaint();
 				Thread.sleep(500);
 				ch=msg.charAt(0);
 				msg=msg.substring(1,msg.length());
@@ -70,9 +75,46 @@ public class Banner extends Applet implements Runnable
 		g.drawString(age,310,250);
 		g.drawString(address,310,300);
 		g.drawString(phno,310,350);
-		
+		//g.drawString(cdac.getWidth(this)+" , "+getHeight(this),310,365);
 		showStatus (x+","+y);
 	}
+	
+	
+ /* @Override
+    public void mouseClicked(MouseEvent e) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+       x = e.getX();
+       y = e.getY();
+    }*/
 }
 /*
 public class MyMouse extends MouseAdapter
