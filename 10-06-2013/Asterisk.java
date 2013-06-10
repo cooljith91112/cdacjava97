6a51bf6880f079a929f96e1022ded1fcf4a1b97d
Indrajith K L <mac91112@gmail.com>
2013-06-10 22:30:26 +0530

    Asterix
    with udayipp!

---
+++ 10-06-2013/Asterisk.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+import java.applet.*;
+/*
+<applet code=Asterisk width=800 height=600>
+</applet>
+*/
+
+public class Asterisk extends Applet 
+{
+
+	public void init()
+	{
+	}
+	
+	public void start()
+	{
+		
+	}
+
+	public void paint(Graphics g)
+	{
+		g.drawLine(104,93,585,488);
+		g.drawLine(112,498,614,88);
+		g.drawLine(88,307,676,287);
+	}
+	
+	public void stop()
+	{
+		
+	}
+	
+	public void destroy()
+	{
+	
+	}
+}
